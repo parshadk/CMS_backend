a4431c87be7f61a057ed778192be995c16ad27fe
Parshad <138670180+parshadk@users.noreply.github.com>
2024-08-02 23:07:44 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,22 +1,36 @@
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
-dotenv.config();
+export const protect = asyncHandler(async (req, res, next) => {
+    let token;
 
-const auth = (req, res, next) => {
-  const token = req.header('x-auth-token');
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
-  }
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
-  }
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+});
+
+export const admin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Not authorized as an admin');
+    }
 };
-
-export default auth;
-
