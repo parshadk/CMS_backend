d9954f0af3856c690938bd9fe8b1f3aa24f10ccd
Parshad <138670180+parshadk@users.noreply.github.com>
2024-08-02 23:06:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 import express from 'express';
-import connectDB from './config/db.js';
-import authRoutes from './routes/auth.js';
-import contentRoutes from './routes/content.js';
 import dotenv from 'dotenv';
-
+import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+import homepageRoutes from './routes/homepageRoutes.js';
+import offeringRoutes from './routes/offeringRoutes.js';
+import solutionRoutes from './routes/solutionRoutes.js';
+import workRoutes from './routes/workRoutes.js';
+import authRoutes from './routes/authRoutes.js'; 
 dotenv.config();
 
-// Connect db
 connectDB();
 
 const app = express();
 
-// Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/content', contentRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/homepage', homepageRoutes);
+app.use('/api/offering', offeringRoutes);
+app.use('/api/solution', solutionRoutes);
+app.use('/api/work', workRoutes);
+app.use('/api/auth', authRoutes); 
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
